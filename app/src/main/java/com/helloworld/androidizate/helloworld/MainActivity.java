a38b32a43060e2d40895438ff06eae635157145e
mtoranzo <mtoranzo@gmail.com>
2017-07-03 16:45:38 -0300

    Click de boton

--- app/src/main/java/com/helloworld/androidizate/helloworld/MainActivity.java
+++ app/src/main/java/com/helloworld/androidizate/helloworld/MainActivity.java
@@ -2,6 +2,11 @@ package com.helloworld.androidizate.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.btnTest);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final TextView txt = (TextView) findViewById(R.id.txtLabel);
+                txt.setText("HOLA");
+            }
+        });
     }
 }
